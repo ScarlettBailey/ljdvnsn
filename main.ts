1e66a6c3ef9501792a474574114799efa2d8564e
ScarlettBailey <78829264+ScarlettBailey@users.noreply.github.com>
2021-03-03 12:33:24 +1300

    new stuff has been added because i am a lengend not you peps

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
-let _4digit = grove.createDisplay(DigitalPin.P0, DigitalPin.P0)
-_4digit.point(false)
-let _4digit2 = grove.createDisplay(DigitalPin.P0, DigitalPin.P0)
 basic.forever(function () {
-	
+    led.plotBarGraph(
+    pins.analogReadPin(AnalogPin.P0),
+    225
+    )
+    basic.pause(10)
 })

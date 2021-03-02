587ad79bc3730c1ed29d7264092566a105e2356c
ScarlettBailey <78829264+ScarlettBailey@users.noreply.github.com>
2021-03-03 12:25:11 +1300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+let _4digit = grove.createDisplay(DigitalPin.P0, DigitalPin.P0)
+_4digit.point(false)
+let _4digit2 = grove.createDisplay(DigitalPin.P0, DigitalPin.P0)
+basic.forever(function () {
+	
+})
